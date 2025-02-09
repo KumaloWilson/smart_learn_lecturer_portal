73d5ab60175b257dfd1c587d47be5da05a6f1d80
KumaloWilson <kumalowilson900@gmail.com>
2025-02-09 09:20:21 +0200

    virtualadding

--- src/views/virtual_classes/virtual_class.tsx
+++ src/views/virtual_classes/virtual_class.tsx
@@ -1,23 +1,12 @@
 import React, { useState } from 'react';
-import { Tabs, Button, Drawer, Form, Input, DatePicker, Select, message } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { Tabs, Drawer, Form, message } from 'antd';
 import { useVirtualClassesManagement } from '../../hooks/virtual_classes/hook';
 import { ClassScheduler } from '../../components/virtual_classes/virtual_class';
 import { virtualClassesAPI } from '../../services/virtual_classes/api';
-import type { Moment } from 'moment';
+import { ClassFormValues, VirtualClassForm } from '../../components/virtual_classes/virtual_class_form';
+import { VirtualClassHeader } from '../../components/virtual_classes/class_tabs';
 
 const { TabPane } = Tabs;
-const { RangePicker } = DatePicker;
-
-interface ClassFormValues {
-    course_id: string;
-    topic_id?: string;
-    title: string;
-    description?: string;
-    timeRange: [Moment, Moment];
-    isRecurring?: boolean;
-    recurrencePattern?: string;
-}
 
 export const VirtualClasses: React.FC<{ lecturerId: string }> = ({ lecturerId }) => {
     const {
@@ -68,16 +57,7 @@ export const VirtualClasses: React.FC<{ lecturerId: string }> = ({ lecturerId })
 
     return (
         <div className="p-6">
-            <div className="flex justify-between mb-6">
-                <h1 className="text-2xl font-bold">Virtual Classes</h1>
-                <Button
-                    type="primary"
-                    icon={<PlusOutlined />}
-                    onClick={() => setIsDrawerVisible(true)}
-                >
-                    Schedule New Class
-                </Button>
-            </div>
+            <VirtualClassHeader onNewClass={() => setIsDrawerVisible(true)} />
 
             <Tabs activeKey={activeTab} onChange={setActiveTab}>
                 <TabPane tab="All Classes" key="all">
@@ -97,80 +77,15 @@ export const VirtualClasses: React.FC<{ lecturerId: string }> = ({ lecturerId })
                 width={500}
                 footer={null}
             >
-                <Form
+                <VirtualClassForm
                     form={form}
+                    isSubmitting={isSubmitting}
+                    lecturerCourses={lecturerCourses}
+                    courseTopics={courseTopics}
+                    selectedCourse={selectedCourse}
+                    onCourseChange={handleCourseChange}
                     onFinish={handleCreateClass}
-                    layout="vertical"
-                    disabled={isSubmitting}
-                >
-                    <Form.Item
-                        name="course_id"
-                        label="Course"
-                        rules={[{ required: true, message: 'Please select a course' }]}
-                    >
-                        <Select
-                            onChange={handleCourseChange}
-                            placeholder="Select a course"
-                        >
-                            {lecturerCourses.map(course => (
-                                <Select.Option key={course.course_id} value={course.course_id}>
-                                    {course.course_name}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </Form.Item>
-
-                    <Form.Item
-                        name="topic_id"
-                        label="Topic"
-                        rules={[{ required: true, message: 'Please select a topic' }]}
-                    >
-                        <Select
-                            placeholder="Select a topic"
-                            disabled={!selectedCourse}
-                        >
-                            {courseTopics.map(topic => (
-                                <Select.Option key={topic.topic_id} value={topic.topic_id}>
-                                    {topic.topic_name}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </Form.Item>
-
-                    <Form.Item
-                        name="title"
-                        label="Title"
-                        rules={[{ required: true, message: 'Please enter a title' }]}
-                    >
-                        <Input />
-                    </Form.Item>
-
-                    <Form.Item name="description" label="Description">
-                        <Input.TextArea />
-                    </Form.Item>
-
-                    <Form.Item
-                        name="timeRange"
-                        label="Class Time"
-                        rules={[{ required: true, message: 'Please select class time' }]}
-                    >
-                        <RangePicker
-                            showTime
-                            format="YYYY-MM-DD HH:mm"
-                        />
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            block
-                            loading={isSubmitting}
-                        >
-                            Schedule Class
-                        </Button>
-                    </Form.Item>
-                </Form>
+                />
             </Drawer>
         </div>
     );
